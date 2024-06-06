1257ad5374413a683b3975867904095b8c03a9fe
Carlos Kuhn <31604991+carlosclaiton@users.noreply.github.com>
2024-06-06 14:48:28 +1000

    adjust to read from games.csv

--- generate_data.py
+++ generate_data.py
@@ -38,15 +38,41 @@ def gen_cot_prompt(moves : str) -> str:
     cot_moves = [f"White: {prev_moves[i]}, Black: {prev_moves[i+1]}" if i + 1 < len(prev_moves) else f"White: {prev_moves[i]}" for i in range(0, len(prev_moves), 2)]
     return cot_moves
 
+# def generate_explanation(client : OpenAI, df : pd.DataFrame, sytem_content : str) -> pd.DataFrame:
+#     return_df = df.copy()
+
+#     for i in range(df.shape[0]):
+#         print(f"[Info] Generating explanation for row {i}")
+#         prev_moves = df.iloc[i]["prev_moves"]
+#         last_move = df.iloc[i]["last_move"]
+
+#         user_prompt_move = gen_cot_prompt(prev_moves, last_move)
+#         user_content_cot = f"Previous move pairs are - \n{user_prompt_move}"
+        
+#         stream = client.chat.completions.create(
+#         model="gpt-3.5-turbo-0125",
+#         messages = [{"role": "system",
+#                      "content": sytem_content},
+#                     {"role": "user",
+#                      "content": user_content_cot}],
+#         stream=True,)
+        
+#         explanation = ""
+#         for chunk in stream:
+#             if chunk.choices[0].delta.content is not None:
+#                 explanation += chunk.choices[0].delta.content
+#         return_df.loc[i, "explanation"] = explanation.strip()
+#     return return_df
+
 def generate_explanation(client : OpenAI, df : pd.DataFrame, sytem_content : str) -> pd.DataFrame:
     return_df = df.copy()
 
     for i in range(df.shape[0]):
         print(f"[Info] Generating explanation for row {i}")
-        prev_moves = df.iloc[i]["prev_moves"]
-        last_move = df.iloc[i]["last_move"]
-
-        user_prompt_move = gen_cot_prompt(prev_moves, last_move)
+        moves = df.iloc[i]["moves"]
+        id_temp = df.index[i]
+        
+        user_prompt_move = gen_cot_prompt(moves)
         user_content_cot = f"Previous move pairs are - \n{user_prompt_move}"
         
         stream = client.chat.completions.create(
@@ -61,12 +87,12 @@ def generate_explanation(client : OpenAI, df : pd.DataFrame, sytem_content : str
         for chunk in stream:
             if chunk.choices[0].delta.content is not None:
                 explanation += chunk.choices[0].delta.content
-        return_df.loc[i, "explanation"] = explanation.strip()
+        return_df.loc[id_temp, "explanation"] = explanation.strip()
     return return_df
 
 def arg_setup() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file", required=True, help="[Required] path to the input data cluster(csv file)")
+    # parser.add_argument("--file", required=True, help="[Required] path to the input data cluster(csv file)")
     parser.add_argument("-i", help = "use a .txt file to pass token")
     parser.add_argument("-mI", action = "store_true", help = "manually enter your token")
     return parserget_file_path
@@ -92,14 +118,23 @@ if __name__ == "__main__":
 
     openai_api_key = get_api_key(args)
 
-    target_file_path = get_file_path(int(args.file))
-    target_df  = load_df(target_file_path)
+    # target_file_path = get_file_path(int(args.file))
+    target_file_path = './data/games.csv'
+    
+    df = load_df(target_file_path)
+    target_df  = df.iloc[:-1]
     # target_df = target_df.head().copy()
 
     # selected prompt
+    # system_content_cot = '''Assume you are a chess master.
+    # You will be provided with a list of chess move pairs in Algebraic Notation where 1st move is by White and 2nd by Black.
+    # Your task is to analyse each pair and then generate the rationale behind each White and Black move and then the last move.
+    # '''
+    
     system_content_cot = '''Assume you are a chess master.
-    You will be provided with a list of chess move pairs in Algebraic Notation where 1st move is by White and 2nd by Black.
-    Your task is to analyse each pair and then generate the rationale behind each White and Black move and then the last move.
+    You will be provided with a list of chess move pairs in Algebraic Notation where 1st move is by White and 2nd by Black and so on.
+    Your task is to analyse each pair and then generate the rationale behind each White and Black move.
+    After the pair based analysis you have to write a summary present a list of used strategies during the game and a reason for the winner.
     '''
 
     target_df = generate_explanation(OpenAI(api_key = openai_api_key), target_df, system_content_cot)
