578b8a83da0ad38fe616415f5d0b027398ac9eb1
Adnan525 <adnaan525@gmail.com>
2024-06-25 09:46:52 +1000

    IMPORTANT - Finetuned model behaviour

---
+++ cognition_framework/test.py
@@ -0,0 +1,3 @@
+from stockfish import Stockfish
+stockfish = Stockfish("/home/s448780/workspace/cognitive_ai/cognition_framework/stockfish/stockfish-ubuntu-x86-64-avx2")
+print(stockfish.get_best_move())
