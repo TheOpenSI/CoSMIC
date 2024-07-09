ed53dd0ac2fabe2a172f308718ddf3b96650e4ef
Adnan525 <adnaan525@gmail.com>
2024-07-09 13:03:32 +1000

    finetune 2 data added

--- cognition_framework/test.py
+++ cognition_framework/test.py
@@ -1,3 +1,8 @@
 from stockfish import Stockfish
 stockfish = Stockfish("/home/s448780/workspace/cognitive_ai/cognition_framework/stockfish/stockfish-ubuntu-x86-64-avx2")
-print(stockfish.get_best_move())
+# print(stockfish.get_best_move())
+FEN = "1Q6/5ppk/3p4/4r3/4q3/2P5/P5PP/R5K1 w - - 3 25"
+stockfish.set_fen_position(FEN)
+print(stockfish.get_parameters())
+
+# print(stockfish.get_best_move())
