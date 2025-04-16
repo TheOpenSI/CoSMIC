48a34049f6040ba79d9656b95591944a88e02380
zohaibhammad <zohaibhammad@gmail.com>
2025-04-16 21:26:58 +1000

    feat: Implemented pipelines in docker-compose and removed chatbot module [cosmic-20]

--- scripts/chatbot/cosmic.sh
+++ scripts/chatbot/cosmic.sh
@@ -2,5 +2,5 @@ echo "Starting Cosmic Chatbot"
 echo "========================"
 echo "Starting Ollama"
 ollama serve > ollama.log 2>&1 &
-echo "Starting Chatbot"
-bash /app/scripts/chatbot/run_chatbot_docker.sh
+# echo "Starting Chatbot"
+# bash /app/scripts/chatbot/run_chatbot_docker.sh
