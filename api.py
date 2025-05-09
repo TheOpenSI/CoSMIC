0f70a59580bd89a01a19bef8e2a3b0de0998310c
zohaibhammad <zohaibhammad@gmail.com>
2025-05-09 17:39:45 +1000

    feat: Implemented new APIs for config update; update docker compose

--- api.py
+++ api.py
@@ -1,12 +1,26 @@
 from datetime import datetime
-from fastapi import FastAPI
+import re
+from fastapi import FastAPI, Request, File, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from src.opensi_cosmic import OpenSICoSMIC
 from pydantic import BaseModel
 import yaml, os, shutil
 import pandas as pd
 from zoneinfo import ZoneInfo
+from typing import Optional
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["*"] to allow all
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+UPLOAD_BASE_DIR = "third_party"
+os.makedirs(UPLOAD_BASE_DIR, exist_ok=True)
 
 class CosmicAPI(BaseModel):
     body: dict
@@ -143,12 +157,140 @@ with open(config_path, "w") as file:
 # Initialize the OpenSICoSMIC class
 opensi_cosmic = OpenSICoSMIC(config_path=config_path)
 
+# All these configs must be given to
+# Open-WebUi/src/lib/components/admin/Settings/Configs.svelte;
+# otherwise set them as Optional[the config name]=default value;
+
+class QueryQnalyserConfig(BaseModel):
+    llm_name: Optional[str] = opensi_cosmic.config['query_analyser']['llm_name']
+    is_quantized: Optional[bool] = opensi_cosmic.config['query_analyser']['is_quantized']
+
+
+class RAGConfig(BaseModel):
+    top_k: Optional[int] = opensi_cosmic.config['rag']['topk']
+    retrieve_score_threshold: Optional[float] = opensi_cosmic.config['rag']['retrieve_score_threshold']
+    vector_db_path: Optional[str] = opensi_cosmic.config['rag']['vector_db_path']
+
+
+class ChessConfig(BaseModel):
+    stockfish_path: Optional[str] = opensi_cosmic.config['chess']['stockfish_path']
+
+
+class OpenAIConfig(BaseModel):
+    api_key: Optional[str] = openai_api_key
+
+class ConfigUpdateForm(BaseModel):
+    llm_name: str
+    is_quantized: bool
+    seed: int
+    doc_directory: str
+    document_path: str
+    service: int
+    sameasabove: Optional[bool] = False
+    query_analyser: QueryQnalyserConfig
+    rag: RAGConfig
+    chess: ChessConfig
+    openai: OpenAIConfig
+
 openai_api_status = opensi_cosmic.check_openai_key()
 
 @app.get("/")
 async def read_root():
     return {"message": "Welcome to the OpenSICoSMIC API"}
+
+@app.get("/config")
+async def get_config():
+    try:
+        result = opensi_cosmic.config
+        config["OPENAI_API_KEY"] = openai_api_key
+
+        return result
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
     
+@app.post("/config/update")
+async def update_config(request: Request, form_data: ConfigUpdateForm):
+    try:
+        # Update all the variables to /app/backend/configs/config_default.yaml.
+        # This will be used by CoSMIC pipeline.
+        # Step 1: read config.yaml
+        with open(config_path, "r") as file:  # was config_default_path
+            config_data = yaml.safe_load(file)
+
+        # Step 2: update the values, and save it as config_default.yaml.
+        config_data["llm_name"] = form_data.llm_name
+        config_data["is_quantized"] = form_data.is_quantized
+        config_data["seed"] = form_data.seed
+        config_data["service"] = form_data.service
+        config_data["query_analyser"]["llm_name"] = form_data.query_analyser.llm_name
+        config_data["query_analyser"]["is_quantized"] = form_data.query_analyser.is_quantized
+        config_data["rag"]["topk"] = form_data.rag.top_k
+        config_data["rag"]["retrieve_score_threshold"] = form_data.rag.retrieve_score_threshold
+        config_data["sameasabove"] = form_data.sameasabove
+
+        # Update only when these paths exist, instead of overwriting by invalid paths.
+        if os.path.exists(form_data.doc_directory):
+            config_data["doc_directory"] = form_data.doc_directory
+
+        if os.path.exists(form_data.document_path):
+            config_data["document_path"] = form_data.document_path
+
+        if os.path.exists(form_data.rag.vector_db_path):
+            config_data["rag"]["vector_db_path"] = form_data.rag.vector_db_path
+
+        if os.path.exists(form_data.chess.stockfish_path):
+            config_data["chess"]["stockfish_path"] = form_data.chess.stockfish_path
+
+        # # Save OpenAI API key to .env instead of displaying in config_updated.yaml.
+        # env_path = "/app/backend/.env"
+
+        # This might not be useful as it is in docker container.
+        if form_data.openai.api_key:
+            os.environ["OPENAI_API_KEY"] = form_data.openai.api_key
+
+        # # Change the root's .env which is shared with .env in this backend container.
+        # dotenv.set_key(env_path, "OPENAI_API_KEY", form_data.openai.api_key)
+
+        # request.app.config = config_data
+        # request.app.config["OPENAI_API_KEY"] = form_data.openai.api_key
+
+        # Save updated configs to config_updated.yaml, instead of overwriting config.yaml.
+        with open(config_path, "w") as file:
+            yaml.safe_dump(config_data, file)
+
+        # return request.app.config
+        return {"status": "success", "message": "Configuration updated successfully"}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+    
+@app.post("/chess/upload")
+async def upload_file(file: UploadFile = File(...)):
+    try:
+
+        if file.content_type != "application/octet-stream":
+            raise HTTPException(status_code=400, detail="Only binary files are allowed.")
+
+        folder_name = file.filename
+        save_dir = os.path.join(UPLOAD_BASE_DIR, folder_name)
+        os.makedirs(save_dir, exist_ok=True)
+
+        save_path = os.path.join(save_dir, file.filename)
+
+        with open(save_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        with open(config_path, "r") as file:  # was config_default_path
+            config_data = yaml.safe_load(file)
+
+        config_data["chess"]["stockfish_path"] = save_path
+
+        with open(config_path, "w") as file:
+            yaml.safe_dump(config_data, file)
+
+        return {"status": "success", "message": f"File saved to {save_path}"}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+
 @app.get("/quit")
 async def quit():
     try:
@@ -182,6 +324,8 @@ async def process_cosmic(data: CosmicAPI):
         user_id = data.body["user"]["id"]
         user_role = data.body["user"]["role"]
         user_email = data.body["user"]["email"]
+
+        print(data.body)
 
         # Set user ID to use a specific vector database.
         # For the same user, the QA instance will not change.
