0c83e8e0ffba7b02b143a1174a8fd3eee5e10b2b
zohaibhammad <zohaibhammad@gmail.com>
2025-06-03 16:49:37 +1000

    feat: Update CORS configuration to allow specified origins from environment variables in CoSMIC and Open-WebUI

--- api.py
+++ api.py
@@ -26,9 +26,22 @@ from utils.statistics import update_statistic_per_query
 
 app = FastAPI()
 
+# To test CORS_ALLOW_ORIGIN locally, you can set something like
+# CORS_ALLOW_ORIGIN=http://localhost:5173;http://localhost:8080
+# in your .env file depending on your frontend port, 8080 or 5173 in this case.
+
+CORS_ALLOW_ORIGIN = [
+    "http://localhost:8080",  # Open-WebUI production server
+    "http://localhost:5173",  # Open-WebUI development server
+]
+
+# Allow CORS for the specified origins
+if os.environ.get("CORS_ALLOW_ORIGIN"):
+    CORS_ALLOW_ORIGIN.extend(os.environ.get("CORS_ALLOW_ORIGIN").split(";"))
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # or ["*"] to allow all
+    allow_origins=CORS_ALLOW_ORIGIN,  # or ["*"] to allow all
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
