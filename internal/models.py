7fd372ed29b05ed544f201cf0a7d93fc5b2b3997
zohaibhammad <zohaibhammad@gmail.com>
2025-06-12 16:40:53 +1000

    fix: Fixed some issues in the models

--- internal/models.py
+++ internal/models.py
@@ -1,23 +1,26 @@
+from google_crc32c import value
 from sqlalchemy import Column, Integer, String, Boolean, Float, DateTime, ForeignKey
 from internal.db import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, unique=True, index=True, autoincrement="auto")
+    openweb_id = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     role = Column(String)
 
 class Service(Base):
     __tablename__ = "services"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, unique=True, index=True, autoincrement="auto")
     title = Column(String)
+    value = Column(Integer, unique=True, index=True)
 
 class Config(Base):
     __tablename__ = "configs"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, unique=True, index=True, autoincrement="ignore_fk")
     user_id = Column(Integer, ForeignKey("users.id"))
     service_id = Column(Integer, ForeignKey("services.id"))
     chess_path = Column(String)
@@ -35,7 +38,7 @@ class Config(Base):
 class Statistic(Base):
     __tablename__ = "statistics"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, unique=True, index=True, autoincrement="ignore_fk")
     user_id = Column(Integer, ForeignKey("users.id"))
     email = Column(String)
     start_date = Column(DateTime)
