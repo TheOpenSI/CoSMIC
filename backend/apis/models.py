09f93e5dd28c1ddfa6e82b37efad9514fd498668
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-03-01 23:48:36 +1100

    fix: add mising columns and configs for Users table
    I don't really care anymore now. Using framework generally are such a
    PITA, especially using framework that is a wrapper over popular
    libraries with nearly 0 docs whatsoever.

--- backend/apis/models.py
+++ backend/apis/models.py
@@ -1,11 +1,32 @@
+### Core modules ###
 from sqlmodel import Field, SQLModel
+from sqlalchemy.schema import PrimaryKeyConstraint, UniqueConstraint
+
+
+### Type hints ###
+
+
+### Internal modules ###
 from .cruds import *
 
 
 class UserBase(SQLModel):
     """docstring for UserBase."""
-    name: str = Field(index=True)
+    # One way of writing custom constraint name:
+    # https://docs.sqlalchemy.org/en/21/orm/declarative_tables.html#orm-declarative-table-configuration
+    __table_args__ = (
+        PrimaryKeyConstraint("id", name="PK_USER_ID"),
+        UniqueConstraint("email", name="UK_USER_EMAIL"),
+    )
+
+    name: str = Field(
+        max_length=100
+    )
     email: str | None = Field(
         default=None,
-        unique=True
+        max_length=256,
+        unique=False # This's kinda funny but there's valid case for doing this
+    )
+    password: str = Field(
+        max_length=256
     )
