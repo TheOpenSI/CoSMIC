2cf0925da7eddc1a8e212ff98a17989c76f073bb
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-03-09 22:51:43 +1100

    exp(opensi_cosmic): attempt to get CoSMIC working on new platform and codebase (Part 3)
    I decided to just disable the logic to transfer user query data over to
    Query Analyser under `/cosmic` endpoint for now. I'll talk about that I
    need to create the API first, and re-build the codebase again so it's
    more manageable for me mostly but for anyone joining the team in the
    future as well. This way, I can have some more weeks to deeply
    investigate our core code and fully migrated over to the new codebase
    and use our interal API endpoints instead. Right now, it's heavily rely
    on hard-coded config file, API & authentication values, and OpenWebUI
    internal API endpoints.
    
    Also, I removed the extra configuration options for each defined volume
    as we don't really do anything that have to touch on those stuff yet.
    Just keep it as default value and it should be alright. Oh, and our
    Ollama service will be rely on the up/down of backend service instead of
    database service.

--- backend/cores/api.py
+++ backend/cores/api.py
@@ -35,7 +35,7 @@ from fastapi import FastAPI
 from ..cores.db import create_db_and_table, create_default_account
 from ..routers.normal_endpoints.root import root_router
 from ..routers.normal_endpoints.home import home_router
-# from ..routers.normal_endpoints.cosmic import cosmic_router
+from ..routers.normal_endpoints.cosmic import cosmic_router
 from ..routers.api_endpoints.users import users_v1_router
 from ..routers.api_endpoints.roles import roles_v1_router
 from ..routers.api_endpoints.chatboxes import chatboxes_v1_router
@@ -62,7 +62,7 @@ cosmic_app: FastAPI = FastAPI(lifespan=lifespan)
 # Normal endpoints
 cosmic_app.include_router(router=root_router)
 cosmic_app.include_router(router=home_router)
-# cosmic_app.include_router(router=cosmic_router)
+cosmic_app.include_router(router=cosmic_router)
 
 
 # API endpoints (V1)
