d96a81edf9645c8d5eaf149e40d817edf5d1d71d
carlosclaiton <carlosclaitonkuhn@gmail.com>
2026-03-12 09:15:40 +0000

    feat: new api post for ollama

--- api.py
+++ api.py
@@ -42,8 +42,8 @@ ollama_client = Client(
 # in your .env file depending on your frontend port, 8080 or 5173 in this case.
 
 CORS_ALLOW_ORIGIN = [
-    "http://localhost:8080",  # Open-WebUI production server
-    "http://localhost:5173",  # Open-WebUI development server
+    "http://localhost:8080",  # Frontend production server
+    "http://localhost:5173",  # Frontend development server
 ]
 
 # Allow CORS for the specified origins
