ba1a3fe2fa2bcfd6df37963356c7b5b99fda7273
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-03-21 23:16:47 +1100

    doc: update README to finalise last migration step to new repo

--- backend/cores/env.py
+++ backend/cores/env.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 def get_env() -> dict[str, str | None]:
-    env_file = (Path(__file__).resolve(strict=True).parent / "fastapi.env")
+    env_file = (Path(__file__).resolve(strict=True).parent / "cosmic_cfg.env")
 
     return dotenv_values(
         dotenv_path=env_file,
