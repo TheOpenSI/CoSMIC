4025039293b5f04d5f3ba739d2bd25307dde5952
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-03-21 23:47:57 +1100

    chore: remove static endpoints so that FE will handle this instead

--- cores/api.py
+++ cores/api.py
@@ -8,8 +8,6 @@ from fastapi import FastAPI
 
 ### Internal modules ###
 from ..cores.db import create_db_and_table, create_default_account
-from ..routers.normal_endpoints.root import root_router
-from ..routers.normal_endpoints.home import home_router
 from ..routers.normal_endpoints.cosmic import cosmic_router
 from ..routers.api_endpoints.users import users_v1_router
 from ..routers.api_endpoints.roles import roles_v1_router
@@ -36,8 +34,6 @@ cosmic_app: FastAPI = FastAPI(lifespan=lifespan)
 
 
 # Normal endpoints
-cosmic_app.include_router(router=root_router)
-cosmic_app.include_router(router=home_router)
 cosmic_app.include_router(router=cosmic_router)
 
 
