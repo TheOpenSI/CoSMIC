8e4c4f26ae7f2e60238efa74e6ea43fbc7bb1dc5
Adnan525 <adnaan525@gmail.com>
2026-03-31 11:36:49 +0000

    tested benchmark with local llm

--- demo.py
+++ demo.py
@@ -23,23 +23,12 @@
 # -------------------------------------------------------------------------------------------------------------
 
 import os
+import argparse
 
 from src.opensi_cosmic import OpenSICoSMIC
 
 # =============================================================================================================
-
-if __name__ == "__main__":
-    # Switch on this to avoid massive warning.
-    os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
-    # Get the file's absolute path.
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    root = f"{current_dir}"
-
-    # Build the system for a specific LLM.
-    config_path = os.path.join(root, "scripts/configs/config.yaml")
-    opensi_cosmic = OpenSICoSMIC(config_path=config_path)
-
+def run_cli(cosmic_instance: OpenSICoSMIC) -> None:
     while True:
         # Get a query from the terminal.
         query = input("[Query] ")
@@ -48,10 +37,47 @@ if __name__ == "__main__":
         if query.lower() in ["quit", "exit"]: break
 
         # Run for each question/query, return the truncated response if applicable.
-        answer, _, _ = opensi_cosmic(query)
+        answer, _, _ = cosmic_instance(query)
 
         # Print the results.
         print(f"[Answer] {answer}")
+
+def run_benchmark(cosmic_instance: OpenSICoSMIC, questions: list[str]) -> None:
+    for question in questions:
+        print(f"[Question] {question}")
+        answer, _, _ = cosmic_instance(question)
+        print(f"[Answer] {answer}")
+        print("-" * 50)
+
+if __name__ == "__main__":
+    parser =  argparse.ArgumentParser(description = "Academic Governance RAG Benchmark")
+    parser.add_argument("--benchmark",
+                        action = "store_true",
+                        default = False,
+                        help = "Run benchmark questions.")
+
+    args = parser.parse_args()
+    
+    # Switch on this to avoid massive warning.
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+    # Get the file's absolute path.
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    root = f"{current_dir}"
+
+    # Benchmark Questions
+    question_file_path = "data/BM_questions/questions.txt"
+    with open(question_file_path, "r") as f:
+        questions = f.read().splitlines()
+
+    # Build the system for a specific LLM.
+    config_path = os.path.join(root, "scripts/configs/config.yaml")
+    opensi_cosmic = OpenSICoSMIC(config_path=config_path)
+
+    if args.benchmark:
+        run_benchmark(opensi_cosmic, questions)
+    else:
+        run_cli(opensi_cosmic)
 
     # Remove memory cached in the system.
     opensi_cosmic.quit()
