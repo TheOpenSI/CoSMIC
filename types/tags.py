257935d539cb34e0183ff023e5135e99546a8290
DeepInDark <114546939+FaceWithDark@users.noreply.github.com>
2026-07-01 15:06:20 +1000

    feat: add global API endpoint tag class

---
+++ types/tags.py
@@ -0,0 +1,19 @@
+### Core modules ###
+from enum import Enum
+
+
+### Type hints ###
+
+
+### Internal modules ###
+
+
+
+"""
+https://fastapi.tiangolo.com/tutorial/path-operation-configuration/?h=enum#tags-with-enums
+"""
+class APITag(Enum):
+    """docstring for APITag."""
+    models  = "Models API Endpoint"
+    cosmic  = "CoSMIC API Endpoint"
+    memory  = "Memory API Endpoint"
