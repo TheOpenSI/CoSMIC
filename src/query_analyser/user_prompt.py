f77b98180a27b33454bad67ab4caceeaebb43aa1
manilesrun <srunmanile@gmail.com>
2026-07-04 10:30:34 +1000

    fix: update user_prompt

--- src/query_analyser/user_prompt.py
+++ src/query_analyser/user_prompt.py
@@ -61,9 +61,9 @@ class QueryAnalyserService(UserPromptBase):
         user_prompt: str = "{0:s}, {1:s}. {2:s}; {3:s}. {4:s}. {5:s}.".format(
             f"Given {self.num_services} services: '{self.service_string}'",
             f"which service can answer the following query? The query is '{question}'",
-            "For instance, if the query is to predict the next chess move, then select service 0",
-            "otherwise, if the query is to generate or modify a code, then select service 2",
-            "otherwise, if the query is about Academic Governance, then select service 4",
+            "For instance, if the query is to predict the next chess move, then select service 1",
+            "otherwise, if the query is to generate or modify a code, then select service 3",
+            "otherwise, if the query is about Academic Governance, then select service 5",
             "Just return which service without any explainations"
         )
 
