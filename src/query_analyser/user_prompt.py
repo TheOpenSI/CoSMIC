0157feec9445e6d56cbdb42aace6622f19f7dc24
carlosclaiton <carlosclaitonkuhn@gmail.com>
2026-07-10 00:36:26 +0000

    fix: spelling on the user_prompt of query analyser

--- src/query_analyser/user_prompt.py
+++ src/query_analyser/user_prompt.py
@@ -64,10 +64,9 @@ class QueryAnalyserService(UserPromptBase):
             f" otherwise, if the query is to generate or modify a code, then select service 3;" \
             f" otherwise, if the query is about Academic Governance, then select service 5."\
             f" otherwise, if the query is a general knowledge question, then select service 4." \
-            f" Just return which service without any explainations."
+            f" Just return which service without any explanations."
 
-        return user_prompt
-
+        return user_prompt   
 
 class QueryAnalyserSystemInfo(QueryAnalyserService):
     def __init__(
