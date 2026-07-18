50e5c008421a5b8bacfaa8b84a8478bfbfef8f33
Adnan525 <adnaan525@gmail.com>
2026-07-18 08:46:39 +0000

    service name fixed

--- src/services/fallBackService.py
+++ src/services/fallBackService.py
@@ -13,16 +13,32 @@ class FallBackService(ServiceBase):
         """
         super().__init__(**kwargs)
 
+
+    def _fix_service_name(self, 
+                          service_name: str) -> str:
+        """
+        Fix the service name to be more user-friendly e.g. "service_1" -> "Service 1".
+
+        Args:
+            service_name (str): the original service name.
+        
+        Returns:
+            fixed_service_name (str): the fixed service name.
+        """
+        return service_name.replace('_', ' ').title()
+
     
     def _get_service_names(self,
                            services: dict[str, dict[str, str]]) -> str:
         """
-        Get the names of the available services.
+        Get the names of the available services excluding the system information service.
 
         Returns:
             service_names (str): names of the available services.
         """
-        return ', '.join([service.get("name") for service in services.values()])
+        return ', '.join([self._fix_service_name(service.get("name")) 
+                          for service in services.values() 
+                          if not "system_information" in service.get("name")])
 
 
     def __call__(self,
@@ -36,7 +52,7 @@ class FallBackService(ServiceBase):
         """
         raw_response = response = (f"I am unable to assist with that request at this time. "
                                    f"It may be outside my current capabilities, or the required service is not enabled. "
-                                   f"For your reference, I currently have access to the following {len(services)} services: "
+                                   f"For your reference, I currently have access to the following {len(services) - 1} service(s): "
                                    f"{self._get_service_names(services)}. "
                                    f"Please feel free to rephrase your request, or contact the OpenSI team for further support.")
 
